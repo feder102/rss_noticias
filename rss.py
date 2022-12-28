28230353948801fb2489d9016b84afe7f6b34b30
Fede Castiglione <feder102@gmail.com>
2022-12-28 11:44:02 -0300

    cambios en rss previos al reset de commit

--- rss.py
+++ rss.py
@@ -4,14 +4,14 @@ import datetime
 import pytz
 
 
-# Obtén el token de acceso de tu bot y el ID de chat de tu canal de Telegram
-TOKEN = '****'
-CHAT_ID = '***'
+# Obt�n el token de acceso de tu bot y el ID de chat de tu canal de Telegram
+TOKEN = '**'
+CHAT_ID = '**'
 
 # Crea una instancia del bot de Telegram utilizando el token de acceso
 bot = telegram.Bot(token=TOKEN)
 
-# Define la funci�n que recuperar� y enviar� los �ltimos art�culos del feed de RSS
+# Define la funci?n que recuperar? y enviar? los ?ltimos art?culos del feed de RSS
 
 
 def send_rss_feed():
@@ -21,7 +21,7 @@ def send_rss_feed():
     # Obtener la fecha actual
     fecha_actual = datetime.datetime.now(tz=pytz.timezone('America/Argentina/San_Juan'))
 
-    # Obtener el día, el mes y la fecha
+    # Obtener el d�a, el mes y la fecha
     # dia = fecha_actual.day
     # mes = fecha_actual.month
     # ano = fecha_actual.year
@@ -37,16 +37,16 @@ def send_rss_feed():
         fecha_formateada = fecha2.strftime("%Y-%m-%d")
         fecha1 = datetime.datetime.strptime(fecha1, "%Y-%m-%d")
         fecha2 = datetime.datetime.strptime(fecha_formateada, "%Y-%m-%d")
-        # Calculamos la diferencia en días entre las dos fechas
+        # Calculamos la diferencia en d�as entre las dos fechas
         diferencia_dias = fecha1 - fecha2
         if(hour == 20):
-            # Envía el título y el enlace del art?culo a trav?s del bot de Telegram
+            # Env�a el t�tulo y el enlace del art?culo a trav?s del bot de Telegram
             if (diferencia_dias.days == 0):
                 if(fecha_hora.hour <= 20 and fecha_hora.hour >= 14):
                     bot.send_message(
                         chat_id=CHAT_ID, text=f"{entry.title}\n{entry.link}")
         elif(hour == 14):
-            # Envía el t?tulo y el enlace del art?culo a trav?s del bot de Telegram
+            # Env�a el t?tulo y el enlace del art?culo a trav?s del bot de Telegram
             if (diferencia_dias.days == 0):
                 if(fecha_hora.hour <= 14 and fecha_hora.hour >= 8):
                     bot.send_message(
@@ -59,6 +59,6 @@ def send_rss_feed():
         else:
             bot.send_message(
                         chat_id=CHAT_ID, text=f"No hay noticias para mostrar")
+            break
 
-# Llama a la función para enviar el feed de RSS
 send_rss_feed()
