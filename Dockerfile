599b7576e807aaf8506ced1ad9f9cb5e7bc17cd3
Fede Castiglione <feder102@gmail.com>
2023-01-02 08:22:47 -0300

    Se agregan template config, se corrige y se mueve rss.py, se agrega codigo para ejecutarse cuando sea

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM public.ecr.aws/lambda/python:3.9
+
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY src/* ./
+
+CMD [ "rss.handler" ]
---
+++ src/rss.py
@@ -0,0 +1,33 @@
+import telegram
+import feedparser
+from datetime import datetime, timedelta
+import pytz
+from config import CHAT_ID, TOKEN
+
+# Crea una instancia del bot de Telegram utilizando el token de acceso
+bot = telegram.Bot(token=TOKEN)
+# Define la funci?n que recuperar? y enviar? los ?ltimos art?culos del feed de RSS
+
+
+def send_rss_feed():
+    # Recupera el feed de RSS utilizando feedparser
+    feed = feedparser.parse(
+        'https://www.tiempodesanjuan.com/rss/pages/home.xml')
+    # Obtener la fecha actual
+    fecha_actual = datetime.now(tz=pytz.timezone('America/Argentina/San_Juan'))    
+    fecha_resta = fecha_actual - timedelta(hours=6)
+
+    for entry in feed.entries[:20]:
+        # Convertimos las fechas a objetos datetime
+        fecha_publicadcion = datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %z")
+        
+        if(fecha_resta <=  fecha_publicadcion and fecha_publicadcion <= fecha_actual):
+            bot.send_message(
+                chat_id=CHAT_ID, text=f"{entry.title}\n{entry.link}")
+        else:
+            break
+            
+def handler(event, context):
+    send_rss_feed()
+
+handler('','')
